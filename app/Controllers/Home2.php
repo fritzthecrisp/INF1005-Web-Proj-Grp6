92338c0cae80ba4d9d9bd0f22c1ffff27f2f2986
Dingyao0 <82993127+Dingyao0@users.noreply.github.com>
2024-03-27 19:43:01 +0800

    Change to predefined data for home

---
+++ app/Controllers/Home2.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ExerciseModel;
+use App\Models\InstanceModel;
+
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $exerciseModel = new ExerciseModel();
+        $workoutsModel = new InstanceModel();
+
+        $exercises = $exerciseModel->getFirst10Exercises();
+        $workout = $workoutsModel->findAll();
+        
+        return view('home', ['exercises' => $exercises, 'workouts' => $workout]);
+    }
+}
