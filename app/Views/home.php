bf9e30ba7196565234e4db7f185420af9f968be6
Dingyao0 <82993127+Dingyao0@users.noreply.github.com>
2024-03-28 21:19:25 +0800

    Fixed the commit

--- app/Views/home.php
+++ app/Views/home.php
@@ -55,27 +55,9 @@
                             <h6 class="card-subtitle mb-2">Level: <?= $exercise['exer_level'] ?></h6>
                             <p class="card-text">Exercise Eqiupment:<?= $exercise['exer_equipment'] ?></p>
                             <button onclick="window.location.href='<?= site_url("exercises/details/{$exercise['exer_id']}") ?>'">View</button>
-    <h1>WELCOME TO UNIFIT!</h1>
-    <h2>TOP EXERCISES</h2>
-    <div class="card-container" id="exercise-container">
-        <?php foreach ($exercises as $exercise) : ?>
-            <?php
-            $exerciseId = $exercise['exer_id'];
-            $exerciseName = $exercise['exer_name'];
-            $ariaLabelExerciseId = "Exercise Number " . $exerciseId ."- ". $exerciseName;
-            ?>
-            <a href="<?= site_url("exercises/details/{$exerciseId}") ?>" class="exercise-link card-link" style="text-decoration: none;" aria-label="<?= $ariaLabelExerciseId ?>">
-                <div class="exercise cards">
-                    <div class="row">
-                        <div class="col-sm-5">
-                            <img class="card-img" src="/img/image.png" alt="<?= "exerciseImg".$exerciseId?>">
-                        </div>
-                        <div class="col-sm-7">
-                            <div class="card-body">
-                                <h3 class="card-title"><?= $exerciseName?></h3>
-                                <h4 class="card-subtitle">Level: <?= $exercise['exer_level'] ?></h4>
-                                <p class="card-text">Exercise Eqiupment: <?= $exercise['exer_equipment'] ?></p>
-                            </div>
+                            <?php
+                            $ariaLabelExerciseId = "Exercise Number " . $exercise['exer_id'] . "- " . $exercise['exer_name'];
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -100,23 +82,9 @@
                             <h6 class="card-subtitle mb-2">Made by:</h6>
                             <p class="card-text"><?= $workout['workout_description'] ?></p>
                             <button onclick="window.location.href='<?= site_url("workout/details/{$workout['workout_id']}") ?>'">View</button>
-            <?php
-            $workoutId = $workout['workout_id'];
-            $workoutName = $workout['workout_name'];
-            $ariaLabelWorkoutId = "Workout Number " . $workoutId . "- ". $workoutName;
-            ?>
-            <a href="<?= site_url("workout/details/{$workoutId}") ?>" class="workout-link card-link" style="text-decoration: none;" aria-label="<?= $ariaLabelWorkoutId ?>">
-                <div class="workout cards">
-                    <div class="row">
-                        <div class="col-sm-5">
-                            <img class="card-img" src="/img/image.png" alt="<?="workoutImg".$workoutId?>">
-                        </div>
-                        <div class="col-sm-7">
-                            <div class="card-body">
-                                <h3 class="card-title"><?= $workoutName ?></h3>
-                                <h4 class="card-subtitle">Made by: </h4>
-                                <p class="card-text"><?= $workout['workout_description'] ?></p>
-                            </div>
+                            <?php
+                            $ariaLabelWorkoutId = "Workout Number " . $workout['workout_id'] . "- " . $workout['workout_name'];
+                            ?>
                         </div>
                     </div>
                 </div>
