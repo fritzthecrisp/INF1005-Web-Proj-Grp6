0f05ca8db84619404fb31b691bd21c2884b95803
Dingyao0 <82993127+Dingyao0@users.noreply.github.com>
2024-03-30 21:49:28 +0800

    min-width is 300px

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -4,7 +4,7 @@
 <head>
     <title>UniFit</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=0.5">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
