2a2acd65df4211f3a954db64b89227c01fb9e0db
fritzthecrisp <zephteoys@gmail.com>
2024-04-04 18:25:10 +0800

    exercise details done

--- app/Models/CustomModel.php
+++ app/Models/CustomModel.php
@@ -13,40 +13,6 @@ class CustomModel
     {
         $this->db = &$db;
     }
-    function where()
-    {
-        // "SELECT *  FROM exercises"
-        return $this->db->table('exercises')
-            ->where(['exer_id >' => 5])
-            ->where(['exer_id <=' => 10])
-            ->orderBy('exer_id', 'DESC')
-            ->get()
-            ->getResult();
-    }
-
-    function getPublicWorkouts()
-    {
-        $cache = \Config\Services::cache();
-        $user_id = 5; // here you will set the user ID
-        
-
-        $builder = $this->db->table('workouts');
-        $builder->join('users', 'workouts.user_id = users.user_id AND users.user_id = ' . $user_id);
-        $workout_results = $builder->where('workout_public', 'Public')->get()->getResult();        
-        
-        
-        $public_workouts = array();
-        foreach ($workout_results as $object) {
-            $public_workouts[] = (array) $object;
-        }
-
-
-
-        // Cache the fetched exercises
-        $cache->save('public_workouts', $public_workouts, 3600); // Cache for 1 hour (3600 seconds)
-        return $public_workouts;
-    }
-
     function fetchPublicWorkouts()
     {
         // "SELECT *  FROM instances"
--- app/Views/exercise_info.php
+++ app/Views/exercise_info.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url('css/others.css') ?>">
 
 <main class="container" style="display: grid;place-items: center;">
-    <img src="/img/image.png" alt="exerciseImg">
-    <h1><?= $exercise['exercise_name'] ?></h1>
+    <img src=<?= $imgURLs . $exercise['exer_images'] . "?raw=true" ?> alt="exerciseImg">
+    <h1><?= $exercise['exer_name'] ?></h1>
     <div class="column">
         <button>INFO</button>
         <button>INSTRUCTIONS</button>
@@ -13,12 +13,17 @@
     <div class="rounded-border">
         <span>Difficulty</span>
         <span></span>
-        <span>Easy</span>
+        <span><?= $exercise['exer_level'] ?></span>
     </div>
     <div class="rounded-border">
         <span>Equipment</span>
         <span></span>
-        <span>Eq Name</span>
+        <span><?= $exercise['exer_equipment'] ?></span>
     </div>
+    <ol>
+    <?php foreach ($exercise['exer_instructions'] as $instruction) {?>
+        <li><?= $instruction ?></li>
+    <?php } ?>   
+</ol>
 </main>
 <?= $this->endSection() ?>
