905658ef045eef30a0236138e195b8f2a41535f0
lewlipop <156882661+lewlipop@users.noreply.github.com>
2024-04-04 23:12:50 +0800

    remove w3 styles

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -6,7 +6,6 @@
     <title>UniFit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=0.5">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -49,13 +48,13 @@
                         } else {
                             // If the user is not logged in (cookie is not set), show login link
                             echo '<li class="nav-item">
-                            <a class="nav-link w3-bar-item w3-button w3-hover-border-black" href="' . site_url("register") . '">SIGN UP</a>
+                            <a class="nav-link" href="' . site_url("register") . '">SIGN UP</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link w3-bar-item w3-button w3-hover-border-black" href="' . site_url("login") . '">LOGIN</a>
+                            <a class="nav-link" href="' . site_url("login") . '">LOGIN</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link w3-bar-item w3-button w3-hover-border-black" href="' . site_url("myWorkout") . '">MY WORKOUT</a>
+                            <a class="nav-link" href="' . site_url("myWorkout") . '">MY WORKOUT</a>
                           </li>
                     ';
                         }
