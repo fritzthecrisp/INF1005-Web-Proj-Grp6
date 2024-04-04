b3a578ea902c294aca98b6cde300523c05cc8105
lewlipop <156882661+lewlipop@users.noreply.github.com>
2024-04-05 03:22:05 +0800

    Add UNIFIT text beside UNIFIT Logo

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -19,7 +19,10 @@
         <?php $session = \Config\Services::session(); ?>
         <nav class="navbar navbar-expand-lg">
             <div class="container">
-                <a class="navbar-brand" href="<?= site_url("/") ?>"><img src="/img/logos.png" alt="logo" id="logo"></a>
+                <a class="navbar-brand" href="<?= site_url("/") ?>">
+                    <img src="/img/logos.png" alt="logo" id="logo">
+                    <span class="logo-unifit title">UNIFIT</span>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
