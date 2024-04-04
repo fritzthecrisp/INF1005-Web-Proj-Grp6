d0fb75a11a84899ac892000c24636328e3e7746d
Dingyao0 <82993127+Dingyao0@users.noreply.github.com>
2024-04-05 02:23:24 +0800

    AboutUs

--- app/Views/about.php
+++ app/Views/about.php
@@ -2,6 +2,77 @@
 
 <?= $this->section('content') ?>
 <main class="container">
-    <h3 class="title">About</h3>
+
+    <h1 class="title">About us</h1>
+    <hr>
+    <p>Welcome to UniFit, where we believe that everyone should have access
+        to tools that empower them to achieve their fitness goals,regardless of their
+        budget or background. As students ourselves, we understand the challenges of
+        balancing academics, social life,and maintaining a healthy lifestyle. That's
+        why we created UniFit – to provide a solution tailored specifically
+        for students who are looking to improve their physical health without breaking
+        the bank.</p>
+
+    <h2 class="title">WHY UNIFIT</h2>
+    <div class="container-fluid">
+        <div class="row gx-3">
+            <div class="col-md-6 col-lg-6">
+                <div class="why-UniFit">
+                    <div class="icon-holder">
+                        <img style="width: 160px;height: auto;" src="https://pngimg.com/d/free_PNG90756.png" alt="">
+                    </div>
+                    <div class="why-UniFit-info">
+                        <h2 class="size-20 white">FREE OF CHARGE</h2>
+                        <p>UniFit offers a comprehensive suite of tools completely
+                            free of charge</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="why-UniFit">
+                    <div class="icon-holder">
+                        <img style="width: 160px;height: auto;" src="https://gymmboxx.com/wp-content/uploads/2023/10/about-icon3.png" alt="">
+                    </div>
+                    <div class="why-UniFit-info">
+                        <h2 class="size-20 white">Dynamic Workout Planning</h2>
+                        <p>Create and customize workout plans
+                            tailored to your goals and schedule, with the flexibility to adjust as needed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="why-UniFit">
+                    <div class="icon-holder">
+                        <img style="width: 160px;height: auto;" src="https://gymmboxx.com/wp-content/uploads/2023/10/about-icon4.png" alt="">
+                    </div>
+                    <div class="why-UniFit-info">
+                        <h2 class="size-20 white">Public or Private Sharing</h2>
+                        <p>Share your workout plans and progress with the
+                            community or keep them private – the choice is yours.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="why-UniFit">
+                    <div class="icon-holder">
+                        <img style="width: 160px;height: auto;" src="https://w7.pngwing.com/pngs/712/530/png-transparent-silhouette-illustration-of-weight-lifter-fitness-centre-computer-icons-physical-exercise-expander-fitness-trainer-fitness-room-gym-gymnastic-health-512-angle-white-physical-fitness.png" alt="">
+                    </div>
+                    <div class="why-UniFit-info">
+                        <h2 class="size-20 white">Workout Library</h2>
+                        <p>Access a comprehensive library of workouts and exercises curated by fitness experts. Whether you're a beginner or advanced athlete, find the perfect routines to target specific muscle groups, improve endurance, or enhance flexibility.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <h2 class="title">Get Started Today</h2>
+    <p>Ready to take control of your fitness journey? Sign up for free and join the
+        UniFit community today. Whether you're a seasoned athlete or just
+        starting out, we're here to support you every step of the way.</p>
+
 </main>
 <?= $this->endSection() ?>
